fb39e1261dae2252f8adab250ccf036aa55995c9
KIRAY <lol@kremlin.ru>
2017-12-12 17:49:46 +0300

    Lesson 5. Clock example.

--- EP.cpp
+++ EP.cpp
@@ -23,24 +23,24 @@ Author:
 
 using namespace sf;
 
-void KeyPress(Sprite& act) {
+void KeyPress(Sprite& act, float& fTime) {
 	if(Keyboard::isKeyPressed(Keyboard::Left)) {
-		act.move(-0.1f, 0.f);
+		act.move(-0.1f*fTime, 0.f);
 		act.setTextureRect(IntRect(0, 96, 96, 96));
 	}
 	
 	if(Keyboard::isKeyPressed(Keyboard::Right)) {
-		act.move(0.1f, 0.f);
+		act.move(0.1f*fTime, 0.f);
 		act.setTextureRect(IntRect(0, 192, 96, 96));
 	}
 	
 	if(Keyboard::isKeyPressed(Keyboard::Up)) {
-		act.move(0.f, -0.1f);
+		act.move(0.f*fTime, -0.1f);
 		act.setTextureRect(IntRect(0, 288, 96, 96));
 	}
 	
 	if(Keyboard::isKeyPressed(Keyboard::Down)) {
-		act.move(0.f, 0.1f);
+		act.move(0.f*fTime, 0.1f);
 		act.setTextureRect(IntRect(0, 0, 96, 96));
 	}
 	
@@ -50,6 +50,8 @@ void KeyPress(Sprite& act) {
 }
 
 void main() {
+	float teleport = 0.f;
+
 	VideoMode mode = VideoMode::getDesktopMode();
 	RenderWindow wnd(VideoMode(640, 480, mode.bitsPerPixel), "TestWnd");
 	
@@ -62,12 +64,24 @@ void main() {
 	actor.setPosition(50, 25);
 	
 	Event event;
+	Clock clock;
 	while(wnd.isOpen()) {
+		float time = clock.getElapsedTime().asMicroseconds();
+		time /= 400;
+		clock.restart();
+		
+		teleport += time;
+		
+		if(teleport > 3000.f) { // ACHTUNG !!!!
+			actor.setPosition(0, 150);
+			teleport = 0.f;
+		}
+	
 		while(wnd.pollEvent(event)) {
 			if(event.type == Event::Closed) wnd.close();
 		}
 		
-		KeyPress(actor);
+		KeyPress(actor, time);
 		
 		wnd.clear();
 		wnd.draw(actor);
